ed2d1c7fa5c28e307f1727f5cf12a389764f3072
W Li <wqli@me.com>
2013-06-21 00:10:36 +0100

    modified:   matlab/make.m

--- matlab/make.m
+++ matlab/make.m
@@ -15,10 +15,10 @@ try
        % mex CFLAGS="-g" libsvmwrite.c
        % mex CFLAGS="-g" train.c linear_model_matlab.c ../l2r_l2_primal_fun.cpp ../l2r_huber_primal_fun.cpp ../linear.cpp ../SAG.cpp "../blas/*.c"
        % mex CFLAGS="-std=c99 -g" predict.c linear_model_matlab.c ../l2r_l2_primal_fun.cpp ../l2r_huber_primal_fun.cpp ../linear.cpp ../SAG.cpp "../blas/*.c"
-		%mex CFLAGS="-x c++ -v" -largeArrayDims libsvmread.c
-		%mex CFLAGS="-x c++ -v" -largeArrayDims libsvmwrite.c
-		mex CFLAGS="-O3 -fslp-vectorize -fslp-vectorize-aggressive -mllvm -bb-vectorize-aligned-only -mllvm -unroll-allow-partial -mllvm -unroll-runtime -march=native -ffast-math" -largeArrayDims train.c linear_model_matlab.c ../l2r_l2_primal_fun.cpp ../l2r_huber_primal_fun.cpp ../linear.cpp ../SAG.cpp "../blas/*.c"
-		mex CFLAGS="-O3 -fslp-vectorize -fslp-vectorize-aggressive -mllvm -bb-vectorize-aligned-only -mllvm -unroll-allow-partial -mllvm -unroll-runtime -march=native -ffast-math" -largeArrayDims predict.c linear_model_matlab.c ../l2r_l2_primal_fun.cpp ../l2r_huber_primal_fun.cpp ../linear.cpp ../SAG.cpp "../blas/*.c"
+	   % mex CFLAGS="-x c++ -v" -largeArrayDims libsvmread.c
+	   % mex CFLAGS="-x c++ -v" -largeArrayDims libsvmwrite.c
+		mex CFLAGS="-O3 -march=native -ffast-math" -largeArrayDims train.c linear_model_matlab.c ../l2r_l2_primal_fun.cpp ../l2r_huber_primal_fun.cpp ../linear.cpp ../SAG.cpp "../blas/*.c"
+		mex CFLAGS="-O3 -march=native -ffast-math" -largeArrayDims predict.c linear_model_matlab.c ../l2r_l2_primal_fun.cpp ../l2r_huber_primal_fun.cpp ../linear.cpp ../SAG.cpp "../blas/*.c"
 	end
 catch
 	fprintf('If make.m fails, please check README about detailed instructions.\n');
